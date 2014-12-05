ac23d47f65cc734c010dc61357e9e11c805a8e59
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-05 16:04:43 +0530

    added layout support

--- package.js
+++ package.js
@@ -10,8 +10,11 @@ Package.onUse(function (api) {
   api.use('templating');
   api.use('reactive-var');
   api.use('reactive-dict');
+  api.use('underscore');
   
   api.addFiles('lib/client/component.js', 'client');
   api.addFiles('lib/client/api.js', 'client');
+  api.addFiles('lib/client/lean.main.html', 'client');
+  api.addFiles('lib/client/lean.main.js', 'client');
   api.export(['LeanComponents']);
 });

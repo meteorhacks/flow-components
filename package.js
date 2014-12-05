5c22611689bc32f7f05dfed48aa0a9c95c24c3c1
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-06 02:18:22 +0530

    version bumped:

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Lean Components',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/meteorhacks/lean-components',
   name: "meteorhacks:lean-components"
 });

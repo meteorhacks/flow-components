70f3a0b164ad410991cd91bb37f2782c72bb9bde
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-05 16:08:51 +0530

    give it a name

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
   summary: 'Lean Components',
   version: '0.0.1',
-  git: 'https://github.com/meteorhacks/lean-components'
+  git: 'https://github.com/meteorhacks/lean-components',
+  name: "meteorhacks:lean-components"
 });
 
 Package.onUse(function (api) {

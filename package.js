675b4aaa11f5b3355099cdb4aad432808a6ca02e
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-10 10:58:25 +0530

    version bumped

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Flow Components',
-  version: '0.0.7',
+  version: '0.0.9',
   git: 'https://github.com/meteorhacks/flow-components',
   name: "meteorhacks:flow-components"
 });

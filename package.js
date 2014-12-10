e541204fa6a85b010b0bfe1c587212bcc65fe910
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-10 15:31:25 +0530

    make states globally available

--- package.js
+++ package.js
@@ -16,6 +16,7 @@ Package.onUse(function (api) {
   
   api.addFiles('lib/client/action_hub.js', 'client');
   api.addFiles('lib/client/component.js', 'client');
+  api.addFiles('lib/client/lookup.js', 'client');
   api.addFiles('lib/client/api.js', 'client');
   api.addFiles('lib/client/main.html', 'client');
   api.export(['FlowComponents']);

16b02e8315e4bdf1fb532c6b4129f821b3ac179a
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-01-13 17:30:54 +0530

    refactored code base and add some features  - removed FlowComponents.renderLayout  - move render function into a separate file  - included FlowComponents.child() api to get a child and    invoke .get(), .child() and getProp()  - .getProp() api to get properities. Now properties are read only

--- package.js
+++ package.js
@@ -13,13 +13,13 @@ Package.onUse(function (api) {
   api.use('reactive-dict');
   api.use('underscore');
   api.use('raix:eventemitter@0.1.1');
-  
+
   api.addFiles('lib/client/utils.js', 'client');
   api.addFiles('lib/client/action_hub.js', 'client');
   api.addFiles('lib/client/component.js', 'client');
   api.addFiles('lib/client/component_instance.js', 'client');
   api.addFiles('lib/client/api.js', 'client');
   api.addFiles('lib/client/lookup.js', 'client');
-  api.addFiles('lib/client/main.html', 'client');
+  api.addFiles('lib/client/render.js', 'client');
   api.export(['FlowComponents']);
 });

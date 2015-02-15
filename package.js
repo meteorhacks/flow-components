25f89ef7fdffa70b0f8d319ea01c49f46e1895ec
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-15 16:21:07 +0530

    Fix typo in package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Flow Components',
-  version: '0.0.30
+  version: '0.0.30',
   git: 'https://github.com/meteorhacks/flow-components',
   name: "meteorhacks:flow-components"
 });
